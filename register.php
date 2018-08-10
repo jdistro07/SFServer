35a71330811b48e5e7b5a2a4a2c463cca4325958
Johndel <vjohndel07@yahoo.com>
2018-08-10 23:08:35 +0800

    registration and login initial UI

---
+++ register.php
@@ -0,0 +1,34 @@
+<html>
+    <header>
+        <title>Account Registration</title>
+    </header>
+
+    <body>
+        <form action="mod_register.php">
+            <input name="fname" type="text" placeholder="First Name"/>
+            <input name="mname" type="text" placeholder="Middle Name"/>
+            <input name="lname" type="text" placeholder="Last Name"/><br>
+            <input name="birthdate" type="date"/><br>
+            <input name="address" type="text" placeholder="Address"/><br>
+            <input name="organization" type="text" placeholder="Organization"/><br>
+            <input name="position" type="text" placeholder="Position"/><br>
+            <input name="username" type="text" placeholder="Username"/><br>
+            <input name="password" type="password" placeholder="Password"/><br>
+            
+            <select>
+                <option value="1">Administrator</option>
+                <option value="2">Teacher</option>
+                <option value="3">Student</option>
+            </select><br>
+
+            <input name="register" type="submit" value="Register"><br>
+        </form>
+        <a href="dashboard.html"><button>Cancel</button></a>
+    </body>
+</html>
+
+<?php
+
+
+
+?>
